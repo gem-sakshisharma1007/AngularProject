059e9a7c7552bb9c86a738d36eea306cd4a358a1
sakshisharma1007 <Sakshi.A.Sharma@geminisolutions.com>
2023-05-14 22:13:17 +0530

    Angular Assignment1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProjectFirst';
-}
+  title = 'Angular Assignment1';
+  show = false;
+  counter=0
+  counter1(){
+    this.counter++;
+
+  };
+  
+  newItem!: string; 
+  items: string[] = [];
+  addItem()
+   {
+    this.items.push(this.newItem);
+    this.newItem = ''; // Clear the input field
+   }
+   isFifthItem(index: number): boolean {
+    return (index + 1) % 5 === 0; // add 1 to index to make it 1-based
+  }
+
+  }
+
+  
+
+  
+
+
